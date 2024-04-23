d4e7f09d1e78a03ef68afd4a95956532f371d77d
Eduardo Reis <eduardo.g.reis167@gmail.com>
2024-04-23 16:23:33 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaMundo.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+public class Aula01OlaMundo {
+    // isso é um comentário de uma linha.
+    /*
+        comentario
+        de multiplas
+        linhas.
+     */
+
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, shor, long e boolean
+        int age = 10;
+        System.out.println("A idade é "+age+" anos");
+
+    }
+}
